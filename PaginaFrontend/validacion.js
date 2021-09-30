997255d310e2b72f952159ade33765e598c1be96
Cristhian David Dorado Riveros <87508851+CristhianDorado@users.noreply.github.com>
2021-09-30 16:57:58 -0500

    Add files via upload

--- PaginaFrontend/validacion.js
+++ PaginaFrontend/validacion.js
@@ -1,32 +1,45 @@
-function ingresar(){
+function ingresar() {
     var usua = document.getElementById("usuario").value;
     var Cont = document.getElementById("contraseña").value;
-    if(usua=="ACE" && Cont =="2021" )
-    {
+    if (usua == "ACE" && Cont == "2021") {
         alert("Usuario y Contraseña Validos");
     }
-    else{
+    else {
         alert("Datos ingresados erroneos , por favo intente de nuevo");
     }
 }
-function google(){
+function google() {
     alert("Ingreso Valido");
 }
 
-function registrar(){
+function registrar() {
     var name = document.getElementById("Nombre").value;
     var email = document.getElementById("Correo Electronico").value;
     var pass = document.getElementById("Contraseña").value;
     var expresion = /\w+@\w+\.+[a-z]/;
-    if(name=="" || email=="" || pass==""){
+    if (name == "" || email == "" || pass == "") {
         alert("Los campos estan vacios");
         return false;
     }
-    else if (!expresion.test(email)){
+    else if (!expresion.test(email)) {
         alert("El correo no es valido");
         return false;
     }
 
-    alert(name+ ", "+ email + ", " + pass );
+    alert(name + ", " + email + ", " + pass);
 
+}
+
+function recuperar() {
+    var expresion = /\w+@\w+\.+[a-z]/;
+    var Coelectronico = document.getElementById("correo").value;
+    if (Coelectronico == "") {
+        alert("El campo esta vacio");
+        return false;
+    }
+    else if (!expresion.test(Coelectronico)) {
+        alert("El correo no es valido");
+        return false;
+    }   
+    alert("Se enviara un correo a "+ Coelectronico +" con las intruciones para recuperar la contraseña.")
 }
