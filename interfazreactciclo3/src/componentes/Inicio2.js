90e5287aa22de08eba8a4690bcc4cf978599de3d
myriam johana <johan3088@hotmail.com>
2021-10-10 18:11:14 -0500

    cambios en react a la aplicacion

--- interfazreactciclo3/src/componentes/Inicio2.js
+++ interfazreactciclo3/src/componentes/Inicio2.js
@@ -1,38 +1,14 @@
 import React from 'react';
 import '../Styles/EstilosInicio.css';
-import iconLogin from '../Assets/Iconos/user50px.png'
+import IconLogin from '../Assets/Iconos/user50px.png'
+import { TableBody } from '@material-ui/core';
 const Inicio = () => {
     return (
 
     <React.Fragment>
-           
-     <div class="cont1">
-        <div class="barraMenu2">
-            <div class="dropdown">
-                  <button class="dropbtn" >Administrador de ventas</button>
-                  <div class="dropdown-content">
-                    <a href="#">Registro</a>
-                    <a href="#">Historial de ventas</a>
-                  </div>
-            </div>
-            <div class="dropdown">
-                  <button class="dropbtn">Administrador de productos</button>
-                  <div class="dropdown-content">
-                    <a href="ventana_registrar_producto.html">Registro</a>
-                    <a href="ventanaInicio.html">Historial de productos</a>
-                  </div>
-            </div>
-            
-            <button class="button button3">Gestion de usuarios/roles</button>
-            
-            <button class="btnMenuLogin" title="LOGIN">
-            <img url= {iconLogin} />
-            </button>
-            
-        </div>
-        
-    </div>
-           
+      
+         <h1 className="h1ventana  body1">Nombre de la empresa</h1> 
+    
     </React.Fragment>
 
     )
---
+++ interfazreactciclo3/src/componentes/Navbar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import '../Styles/EstilosInicio.css';
+import IconLogin from '../Assets/Iconos/user50px.png'
+import { TableBody } from '@material-ui/core';
+const Barradenavegacion = () => {
+    return (
+
+    <React.Fragment>
+       
+     <div className="cont1">
+        <div className="barraMenu2">
+            <div className="dropdown">
+                  <button className="dropbtn" >Administrador de ventas</button>
+                  <div className="dropdown-content">
+                    <a href="#">Registro</a>
+                    <a href="#">HisNtorial de ventas</a>
+                  </div>
+            </div>
+            <div className ="dropdown">
+                  <button className ="dropbtn">Administrador de productos</button>
+                  <div className="dropdown-content">
+                    <a href="ventana_registrar_producto.html">Registro</a>
+                    <a href="ventanaInicio.html">Historial de productos</a>
+                  </div>
+            </div>
+            
+            <button className="buttonmy button3_1">Gestion de usuarios/roles</button>
+             
+            <button class="btnMenuLogin" title="LOGIN">
+            <img src= {IconLogin}/>
+            </button>
+          
+        </div>
+        
+      </div>
+      
+           
+    </React.Fragment>
+
+    )
+
+}
+
+
+
+export default Barradenavegacion;
